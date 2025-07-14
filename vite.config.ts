4ddb94608a17e74127d231792ae42ef57bd56dbd
codingrot17 <ademolaemmanuel645@gmail.com>
2025-07-14 08:53:56 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
